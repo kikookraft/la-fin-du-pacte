f2d6a011fe1c127649eb549fe6e708de607581d9
kikookraft <kikookraft.teamcraftpgmail.com>
2020-11-11 17:30:17 +0100

    correction/ vidéo trop grandes

--- level.py
+++ level.py
@@ -5,7 +5,7 @@ from moviepy.editor import *
 from score import Score
 import sys
 class Lvl:
- 
+
     screen = pygame.display.set_mode((1080, 720))
 
     # def __init__(self):
@@ -55,6 +55,7 @@ class Lvl:
         screen.fill((0,0,0))
         background = pygame.image.load("assets/death.jpg").convert()
         screen.blit(background,(0,0))
+        #afficher raison de mort
         button_text = font.render(raison, True, (255, 25, 25))
         text_rect = button_text.get_rect(center=(540, 360+150))
         screen.blit(button_text,text_rect)
@@ -63,7 +64,7 @@ class Lvl:
         if tmp == 1:
             return 1
 
-    def wait(self, wait):
+    def wait(self, wait): #fonction qui sert a attendre sans bloquer la fenetre et qui permet de skipper (avec 's')
         for i in range(int(wait)):
             pygame.time.wait(10)
             try:
@@ -77,9 +78,9 @@ class Lvl:
             except:
                 pass
     
-    def choice(self, screen, line):
+    def choice(self, screen, line): #fonction pour generer des choix
         data_choice = line[7:-1]
-        show_choice = tuple(data_choice.split(', '))
+        show_choice = tuple(data_choice.split('; '))
         nbchoix = int(show_choice[0])
         choix1 = show_choice[1]
         choix2 = show_choice[2]
@@ -112,24 +113,24 @@ class Lvl:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE :
                     return 0
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_q or event.type == pygame.KEYDOWN and event.key == pygame.K_a or event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_q or event.type == pygame.KEYDOWN and event.key == pygame.K_a or event.type == pygame.KEYDOWN and event.key == pygame.K_1 or event.type == pygame.KEYDOWN and event.key == pygame.K_KP_1:
                     pygame.draw.rect(screen, (255,255,255), (8, 608, 344, 104), 6)
                     pygame.draw.rect(screen, (0,0,0), (7, 607, 345, 105), 2)
                     choice = False
                     return 1
                 if nbchoix == 2:
-                    if event.type == pygame.KEYDOWN and event.key == pygame.K_b or event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_b or event.type == pygame.KEYDOWN and event.key == pygame.K_2 or event.type == pygame.KEYDOWN and event.key == pygame.K_KP_2:
                         pygame.draw.rect(screen, (255,255,255), (728, 608, 344, 104), 6)
                         pygame.draw.rect(screen, (0,0,0), (727, 607, 345, 105), 2)
                         choice = False
                         return 2
                 if nbchoix == 3:
-                    if event.type == pygame.KEYDOWN and event.key == pygame.K_b or event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_b or event.type == pygame.KEYDOWN and event.key == pygame.K_2 or event.type == pygame.KEYDOWN and event.key == pygame.K_KP_2:
                         pygame.draw.rect(screen, (255,255,255), (368, 608, 344, 104), 6)
                         pygame.draw.rect(screen, (0,0,0), (367, 607, 345, 105), 2)
                         choice = False
                         return 2
-                    if event.type == pygame.KEYDOWN and event.key == pygame.K_c or event.type == pygame.KEYDOWN and event.key == pygame.K_3:
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_c or event.type == pygame.KEYDOWN and event.key == pygame.K_3 or event.type == pygame.KEYDOWN and event.key == pygame.K_KP_3:
                         pygame.draw.rect(screen, (255,255,255), (728, 608, 344, 104), 6)
                         pygame.draw.rect(screen, (0,0,0), (727, 607, 345, 105), 2)
                         choice = False
@@ -144,38 +145,44 @@ class Lvl:
         init = 0
         initdial = 0
         #recherche des fichiers du niveau
-        if name == "Zach":
-            if level == 1:
-                file = "nvx/nvx1.txt"
-            elif level == 2:
-                file = "nvx/nvx2.txt"
-            elif level == 3:
-                file = "nvx/nvx3.txt"
-            elif level == 4:
-                file = "nvx/nvx4.txt"
-        elif name == "Angela":
-            if level == 1:
-                file = "nvx/nvx1A.txt"
-            elif level == 2:
-                file = "nvx/nvx2A.txt"
-            elif level == 3:
-                file = "nvx/nvx3A.txt"
-            elif level == 4:
-                file = "nvx/nvx4A.txt"
-        elif name == "UtopiaJr":
-            if level == 1:
-                file = "nvx/nvxU.txt"
-            elif level == 2:
-                file = "nvx/nvxUU.txt"
-            elif level == 3:
-                file = "nvx/nvxUUU.txt"
-            elif level == 4:
-                file = "nvx/nvxX.txt"
-        else:
-            file = "nvx/tuto.txt"
+        try:
+            if name == "Zach":
+                if level == 1:
+                    file = "nvx/nvx1.txt"
+                elif level == 2:
+                    file = "nvx/nvx2.txt"
+                elif level == 3:
+                    file = "nvx/nvx3.txt"
+                elif level == 4:
+                    file = "nvx/nvx4.txt"
+            elif name == "Angela":
+                if level == 1:
+                    file = "nvx/nvx1A.txt"
+                elif level == 2:
+                    file = "nvx/nvx2A.txt"
+                elif level == 3:
+                    file = "nvx/nvx3A.txt"
+                elif level == 4:
+                    file = "nvx/nvx4A.txt"
+            elif name == "UtopiaJr":
+                if level == 1:
+                    file = "nvx/nvxU.txt"
+                elif level == 2:
+                    file = "nvx/nvxUU.txt"
+                elif level == 3:
+                    file = "nvx/nvxUUU.txt"
+                elif level == 4:
+                    file = "nvx/nvxX.txt"
+            else:
+                file = "nvx/tuto.txt"
+        except:
+            print("Il manque certain niveau !\nVeuiller verifier les fichiers du jeux !")
+            pygame.quit()
+            sys.quit()
         #ouverture du fichier
         f = open(str(file), 'r', encoding='utf-8')
         line = f.readline().rstrip('\n')
+        sound = None
         while line:
             if int(dialog) != 0 and init != 0 and initdial == 0:
                 towait = "markr ("+str(dialog)+")"
@@ -246,8 +253,12 @@ class Lvl:
                 
                 #vidéo
                 elif line[0:5] == "video":
+                    try:
+                        sound.stop()
+                    except:
+                        pass
                     video = str(line[7:-1])
-                    clip = VideoFileClip(video)
+                    clip = VideoFileClip(video).resize((1080,720))
                     clip.preview(fps=30)
                     
                 #generation des dialogues
@@ -258,7 +269,7 @@ class Lvl:
                     y = 21
                     nb = 0
                     data_text = line[7:-1]
-                    showed_text = tuple(data_text.split(', '))
+                    showed_text = tuple(data_text.split('; '))
                     for number in range(int(showed_text[0])):
                         fnd = pygame.Surface((1040, 48))
                         fnd.set_alpha(200)  
@@ -306,7 +317,7 @@ class Lvl:
                     if rslt == 0:
                         return level, dialog, name
                     ln = line[7:-1]
-                    tpl = tuple(ln.split(', '))
+                    tpl = tuple(ln.split('; '))
                     towait = str(tpl[int(tpl[0])+rslt])
 
             pygame.display.flip()
@@ -315,102 +326,3 @@ class Lvl:
                 return level, dialog, name
             if towait == "None":
                 line = f.readline().rstrip('\n')
-
-                   
-            
-
-
-
-
-        # self.test.initialisation(screen, 1, name)
-        # nvx = self.test.play(screen, level, dialog, name)
-
-        # if level == 0 or name == 'None':
-        #     try:
-        #         with open("data/start.txt"):
-        #             try:
-        #                 with open("data/end.txt"):
-        #                     nvx = self.tuto.choiceunlocked()
-        #             except IOError:
-        #                 nvx = self.tuto.choicelocked()
-        #     except IOError:
-        #         self.tuto.initialisation(screen ,1)
-        #         nvx = self.tuto.tuto(screen)
-
-        # elif level == 1:
-        #     if name == "Zach":
-        #         self.nvx1.initialisation(screen, 1, name)
-        #         nvx = self.nvx1.play(screen, level, dialog, name)
-        #     elif name == "Angela":
-        #         self.nvx1A.initialisation(screen, 1, name)
-        #         nvx = self.nvx1A.play(screen, level, dialog, name)
-        #     elif name == "UtopiaJr":
-        #         self.nvxU.initialisation('nvx/nvxU/init.txt', 1, name, 1)
-        #         nvx = self.nvxU.play(screen, level, dialog, name)
-
-        # elif level == 2:
-        #     if name == "Zach":
-        #         self.nvx2.initialisation3(screen, 1, name)
-        #         nvx = self.nvx2.play(screen, level, dialog, name)
-        #     elif name == "Angela":
-        #         try:
-        #             with open("nvx/nvx1A/player.txt"):
-        #                 self.nvx2A2.initialisation('nvx/nvx2A2/init.txt', 1, name, 1)
-        #                 nvx = self.nvx2A2.play(screen, level, dialog, name)
-        #         except IOError:
-        #             self.nvx2A1.initialisation('nvx/nvx2A1/init.txt', 1, name, 1)
-        #             nvx = self.nvx2A1.play(screen, level, dialog, name)
-        #     elif name == "UtopiaJr":
-        #         self.nvxUU.initialisation('nvx/nvxUU/init.txt', 1, name, 1)
-        #         nvx = self.nvxUU.play(screen, level, dialog, name)
-
-        # elif level == 3: #le nvx3 a une structure différente
-        #     if name == "Zach":
-        #         self.nvx3.initialisation("nvx/nvx3/init.txt", 1, name)
-        #         nvx = self.nvx3.play(screen, level, dialog, name)
-        #     elif name == "Angela":
-        #         self.nvx3A.initialisation("nvx/nvx3A/init.txt", 1, name)
-        #         nvx = self.nvx3A.play(screen, level, dialog, name)
-        #     elif name == "UtopiaJr":
-        #         self.nvxUUU.initialisation('nvx/nvxUUU/init.txt', 1, name, 1)
-        #         nvx = self.nvxUUU.play(screen, level, dialog, name)
-
-        # elif level == 4:
-        #     if name == "Zach":
-        #         self.nvx4.initialisation("nvx/nvx4/init.txt", 1, name)
-        #         nvx == self.nvx4.play(screen, level, dialog, name)
-        #     elif name == "Angela":
-        #         self.nvx4A.initialisation("nvx/nvx4A/init.txt", 1, name)
-        #         nvx == self.nvx4A.play(screen, level, dialog, name)
-
-        # elif level == 41:
-        #     self.nvx41.initialisation("nvx/nvx41/init.txt", 1, name)
-        #     nvx = self.nvx41.play(screen, level, dialog, name)
-
-        # elif level == 5:
-        #     self.nvx4A.initialisation("nvx/nvx4A/init.txt", 1, name)
-        #     nvx = self.nvx4A.play(screen, level, dialog, name)
-
-        # elif level == 666:
-        #     self.nvxX.initialisation("nvx/nvxX/init.txt", 1, name)
-        #     nvx = self.nvxX.play(screen, level, dialog, name)
-
-        # elif level == 441:
-        #     self.nvx4AR.initialisation("nvx/nvx4AR/init.txt", 1, name)
-        #     nvx = self.nvx4AR.play(screen, level, dialog, name)
-
-
-
-        # if nvx[0] == 100:
-        #     print("Tu as débloqué le troisieme personnage !")
-        #     with open("data/end.txt", 'a') as ed:
-        #         ed.write("\nTu as terminé une histoire !")
-        #     pygame.time.wait(1500)
-        #     return 0, 0, "None", 1, 0
-        # if nvx[0] == 99999:
-        #     print("Tu as terminé le jeux !")
-        #     clip = VideoFileClip("assets/movies/credit.mp4")
-        #     clip.preview(fps=30)
-        #     return 0, 0, "None", 1, 0
-        # return nvx[0], nvx[1], nvx[2], nvx[3], nvx[4]
-
